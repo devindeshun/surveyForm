2a61bab9a2b11e7f39df658031f08a11730d5ae2
devindshun <devin.dshun@gmail.com>
2020-05-14 23:46:02 -0500

    Added JS to practice DOM manipulation and nodes.

---
+++ js/code.js
@@ -0,0 +1,21 @@
+let thanks = document.querySelector(".thankyou");
+
+let msg = document.createElement("h2");
+msg.textContent = "Thank you for using my site!";
+
+thanks.appendChild(msg);
+
+thanks.style.position = "absolute";
+thanks.style.color = "white";
+thanks.style.width = "100vw";
+thanks.style.height = "120vh";
+thanks.style.background = "rgb(58, 58, 158)";
+thanks.style.top = "0";
+thanks.style.left = "0px";
+thanks.style.transition = "ease-out all 3s";
+
+
+msg.style.position = "absolute";
+msg.style.top = "50vh";
+msg.style.left = "50%";
+msg.style.transform = "translate(-50%)";
